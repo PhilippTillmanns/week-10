ee2d1778f1646ba22f348a295d574cb7c650f808
BuildTools <unconfigured@null.spigotmc.org>
2023-07-03 20:44:01 +0200

    first push :)

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUjUTa1VRuqDWXnjMweVAH_nd0HNwowzw",
+  authDomain: "week-10-a9eb2.firebaseapp.com",
+  projectId: "week-10-a9eb2",
+  storageBucket: "week-10-a9eb2.appspot.com",
+  messagingSenderId: "1047189548434",
+  appId: "1:1047189548434:web:e31312fb15ecf8ce55254b",
+  measurementId: "G-43PH0C3VMY"
+};
+
+export const app = initializeApp(firebaseConfig);
