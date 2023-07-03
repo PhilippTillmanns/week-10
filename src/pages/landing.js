41af3fd40b4376a20fa178e23220b776453c7184
ronpeschel <ron.peschel@online.de>
2023-07-03 23:06:54 +0200

    added responsiveness to placement of landing

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -6,9 +6,9 @@ function Landing() {
   const imageURL = "https://images.pexels.com/photos/4203100/pexels-photo-4203100.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   
   return (
-    <div className="text-center bg-cover bg-center h-screen flex items-center" style={{ backgroundImage:`url(${image})` }} >
-      <div className="text-left fixed left-40 w-7/12">
-        <div className="text-6xl mb-4 text-clrDarkBlue">What is it that you want to accomplish this week?</div>
+    <div className="text-center bg-cover bg-center h-screen flex items-center" style={{ backgroundImage:`url(${image})`}}  >
+      <div className="text-left fixed left-[10%] w-7/12">
+        <div className="font-semibold text-6xl mb-4 text-clrDarkBlue">What is it that you want to accomplish this week?</div>
         <div className="text-clrLightBlue mb-9 text-2xl">Create something of precise definition. Something that will guide you through your week like nothing else.</div>
 
         <button className="hover:bg-clrLightPurple hover:text-clrDarkBlue duration-300 ease-in-out rounded-full text-3xl bg-clrMidBlue text-clrLightPurple px-8 py-3" onClick={() => SignInWithGoogle()}> Start Now</button>
