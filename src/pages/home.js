edc4d73a80933abec7f8aeaeccf3a7ca70941efa
BuildTools <unconfigured@null.spigotmc.org>
2023-07-04 18:24:26 +0200

    bisschen an custom user info geworked

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { SignOut } from '../components/HandleLoginAndLogout'
 
 function Home() {
-  return (
-    <div>
-        <h1>Logged In!</h1>
-        <button onClick={() => SignOut()}>Sign Out</button>
-    </div>
-  )
+      return (
+        <div>
+            <h1>Logged In!</h1>
+            <button onClick={() => SignOut()}>Sign Out</button>
+        </div>
+      )
 }
 
 export default Home
