3f0ab0afa230a5d4a07c064bdcee316a8dd2faad
ronpeschel <ron.peschel@online.de>
2023-07-04 20:10:51 +0200

    added a first loading screen, not sure if it works though

---
+++ src/pages/loadingScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loadingScreen() {
+  return (
+    <div className=' justify-center bg-purple'>
+        <loadingScreen/>
+    </div>
+  )
+}
+
+export default loadingScreen
