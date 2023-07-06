af8726d44f4d7cf25e8419d850adf7ac408e03f3
ronpeschel <ron.peschel@online.de>
2023-07-06 16:47:38 +0200

    added go back button to create user page

--- src/pages/CreateUser.js
+++ src/pages/CreateUser.js
@@ -3,6 +3,8 @@ import image from "../images/pexels-alex-conchillos-3745234.jpg"
 import './pages-css/home.css'
 import GetUserData, { CreateUserInformation } from '../components/HandleUserInformation';
 import { activeUser } from '../config/firebase';
+import { ReactComponent as IconArrowBackCircle } from '../components/icons';
+import { ReactComponent as ArrowLeft } from '../icons/IonArrowBackCircle.svg';
 
 
 function CreateUser({setIsCreatingUser}) {
@@ -45,11 +47,16 @@ function CreateUser({setIsCreatingUser}) {
                 
                 <button className=' hover:underline absolute right-0 bottom-[-40px] font-semibold' onClick={() => GetUserInputs()}>Submit</button>
                 
+                
             </div>
             
+            
         </div>
-        <div></div>
+        <button>
+          <svg className=' absolute bottom-4 left-2/4' xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 512 512"><path fill="currentColor" d="M48 256c0 114.87 93.13 208 208 208s208-93.13 208-208S370.87 48 256 48S48 141.13 48 256Zm212.65-91.36a16 16 0 0 1 .09 22.63L208.42 240H342a16 16 0 0 1 0 32H208.42l52.32 52.73A16 16 0 1 1 238 347.27l-79.39-80a16 16 0 0 1 0-22.54l79.39-80a16 16 0 0 1 22.65-.09Z"/></svg>
         
+        </button>
+       
 
     </div>
   )
